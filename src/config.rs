e18d9b4ef95d434c10e42d793c1c1c2a1d97cc3c
EashanC23 <108442887+EashanC23@users.noreply.github.com>
2025-02-14 04:08:43 -0500

    Refactored main and implemented config file

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,69 @@
-extern crate dirs;
+use serde::{Deserialize, Serialize};
+use std::collections::{HashMap, HashSet};
+use std::fs;
+use std::path::PathBuf;
+use std::{env, fmt};
 
-pub struct ConfigData {
-    folders: Option<Vec<String>>,
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Config {
+    pub categories: HashMap<String, HashSet<String>>,
 }
 
-impl ConfigData {
-    pub fn config_read() {
-        let config_dir = dirs::home_dir().unwrap().push(".config/dl_sort");
-        println!("{:?}", config_dir);
+impl Config {
+    pub fn load() -> Result<Self, Box<dyn std::error::Error>> {
+        let config_dir = env::var("HOME")
+            .map(PathBuf::from)
+            .or_else(|_| env::var("USERPROFILE").map(PathBuf::from))?;
+
+        let path = config_dir.join(".config/dl_sort/config.yaml");
+
+        if !path.exists() {
+            return Err(format!("Config file not found: {:?}", path).into());
+        }
+
+        let file_sring = fs::read_to_string(&path)?;
+        let ret: Config = serde_yaml::from_str(&file_sring)?;
+
+        Ok(ret)
     }
-    // add code here
+    pub fn load_default() -> Result<Self, Box<dyn std::error::Error>> {
+        let config_yaml: &str = r#"categories:
+                                      Audios:
+                                        - mp3
+                                        - wav
+                                        - flac
+                                      Compressed:
+                                        - zip
+                                        - rar
+                                        - tar
+                                      Developments:
+                                        - rs
+                                        - js
+                                        - java
+                                        - html
+                                      PDFs:
+                                        - pdf
+                                        - txt
+                                        - docx
+                                      Photos:
+                                        - jpg
+                                        - png
+                                        - jpeg
+                                        - webp
+                                      Videos:
+                                        - mp4
+                                        - mov
+                                        - mkv
+                                    "#;
+        let ret: Config = serde_yaml::from_str(config_yaml).unwrap();
+        Ok(ret)
+    }
+}
+impl fmt::Display for Config {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match serde_yaml::to_string(self) {
+            Ok(yaml) => write!(f, "{}", yaml),
+            Err(_) => Err(fmt::Error),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,68 @@
 mod config;
 use std::path::{Path, PathBuf};
-#[warn(non_snake_case)]
 use std::{fs, io};
-extern crate dirs;
 
 fn main() -> io::Result<()> {
-    let a = config::ConfigData;
-    let user_downloads = match dirs::download_dir() {
-        Some(path) => path,
-        None => {
-            return Err(io::Error::new(
-                io::ErrorKind::NotFound,
-                "Cannot locate downloads folder",
-            ));
+    let user_downloads = dirs::download_dir()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Cannot locate downloads folder"))?;
+
+    let config = match config::Config::load() {
+        Ok(cfg) => cfg,
+        Err(e) => {
+            eprintln!("Failed to load config, {}", e);
+            config::Config::load_default().unwrap()
         }
     };
 
-    let required_folders = [
-        "Photos",
-        "Videos",
-        "PDFs",
-        "Developments",
-        "Audios",
-        "Compressed",
-    ];
-    let dl_folder: Vec<_> = fs::read_dir(user_downloads.clone())?
-        .filter_map(|res| {
-            if let Ok(entry) = res {
-                let path = entry.path();
-                if path.is_dir() {
-                    path.file_name()
-                        .map(|name| name.to_string_lossy().into_owned())
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
-        .collect();
-    for i in (0..required_folders.len()).rev() {
-        if !dl_folder.contains(&required_folders[i].to_string()) {
-            let p = user_downloads.join(&required_folders[i].to_string());
-            match fs::create_dir_all(p) {
-                Ok(_) => (),
-                Err(e) => {
-                    return Err(io::Error::new(io::ErrorKind::NotFound, e));
-                }
-            };
+    let required_folders: Vec<String> = config.categories.keys().cloned().collect();
+
+    for folder in &required_folders {
+        let path = user_downloads.join(folder);
+        if !path.exists() {
+            fs::create_dir_all(&path)?;
         }
     }
 
-    let audio_exts = ["mp3", "m4a", "aac", "flac", "wav"];
-    let image_exts = ["jpg", "png", "heic", "jpeg"];
-    let video_exts = ["mp4", "mov", "webm", "mkv", "avi"];
-    let dev_exts = ["rs", "js", "jsx", "css", "java", "jar", "html"];
-    let pdf_exts = ["pdf", "txt", "md", "docx", "epub"];
-    let compressed_exts = ["zip", "iso", "rar", "ipa"];
+    let dls = fs::read_dir(&user_downloads)?;
+    for file_entry in dls {
+        let file_path = file_entry?.path();
+        if let Some(ext) = file_path.extension().and_then(|e| e.to_str()) {
+            let ext = ext.to_lowercase();
+            let file_name = file_path.file_name().unwrap().to_string_lossy().to_string();
 
-    let handle_each = |file_name: &str, file_path: &PathBuf, folder: &str| -> io::Result<()> {
-        let mut to_path: PathBuf = user_downloads.join(folder).join(file_name.clone());
-        if Path::exists(&to_path) {
-            let mut i: i16 = 1;
-            while Path::exists(
-                PathBuf::from(format!(
-                    "{}/{}/{} {}.{}",
-                    &user_downloads.to_string_lossy(),
-                    folder,
-                    &to_path.file_stem().unwrap().to_string_lossy(),
-                    i,
-                    &to_path.extension().unwrap().to_string_lossy()
-                ))
-                .as_path(),
-            ) {
-                i += 1;
-            }
-            let concat_path = format!(
-                "{}/{}/{} {}.{}",
-                &user_downloads.to_string_lossy(),
-                folder,
-                to_path.file_stem().unwrap().to_string_lossy(),
-                i,
-                to_path.extension().unwrap().to_string_lossy()
-            );
-            println!("AAAAAAA {}", concat_path);
-            to_path = PathBuf::from(concat_path);
-            println!("{} already exists ! Renaming to {:?}.", file_name, to_path);
-        }
-        fs::rename(file_path, to_path)?;
-        println!("Moved {} to {}", file_name, folder);
-        Ok(())
-    };
-
-    let dls = fs::read_dir(user_downloads.clone())?;
-    for file_path in dls {
-        let file_path = file_path?.path();
-        if let Some(ex) = file_path.extension().and_then(|e| e.to_str()) {
-            // println!("{:?}", e);
-            let ext = ex.clone().to_lowercase();
-            let file_name = file_path.file_name().unwrap().to_string_lossy();
-            if audio_exts.contains(&ext.as_str()) {
-                handle_each(&file_name, &file_path, "Audios")?;
-            } else if image_exts.contains(&ext.as_str()) {
-                handle_each(&file_name, &file_path, "Photos")?;
-            } else if video_exts.contains(&ext.as_str()) {
-                handle_each(&file_name, &file_path, "Videos")?;
-            } else if dev_exts.contains(&ext.as_str()) {
-                handle_each(&file_name, &file_path, "Developments")?;
-            } else if pdf_exts.contains(&ext.as_str()) {
-                handle_each(&file_name, &file_path, "PDFs")?;
-            } else if compressed_exts.contains(&ext.as_str()) {
-                handle_each(&file_name, &file_path, "Compressed")?
+            for (folder, extensions) in &config.categories {
+                if extensions.contains(&ext) {
+                    move_file(&file_path, &file_name, &user_downloads, folder)?;
+                    break;
+                }
             }
         }
     }
 
+    Ok(())
+}
+
+fn move_file(
+    file_path: &PathBuf,
+    file_name: &str,
+    downloads_dir: &Path,
+    folder: &str,
+) -> io::Result<()> {
+    let mut destination = downloads_dir.join(folder).join(file_name);
+
+    let mut counter = 1;
+    while destination.exists() {
+        let new_name = format!(
+            "{} {}.{}",
+            file_path.file_stem().unwrap().to_string_lossy(),
+            counter,
+            file_path.extension().unwrap().to_string_lossy()
+        );
+        destination = downloads_dir.join(folder).join(new_name);
+        counter += 1;
+    }
+
+    fs::rename(file_path, &destination)?;
+    println!("Moved {} to {}", file_name, folder);
     Ok(())
 }
